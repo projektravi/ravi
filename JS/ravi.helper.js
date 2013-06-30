3369b7872b166b677cb4e11e6aa7e65da52161af
Maik <maik-lindner@gmx.de>
2013-06-30 18:46:55 +0200

    Zusammenfassung aller vorhandenen Ergebnisse: - Einstiegsseite und Stylesheet von Günay - Letzten Stand von Raum- und Datumsauswahl inkludiert - Code aus Startseite ausgelagert (Javascript und Stylesheet) - Drittanbieter-Dateien herunter geladen und bereit gestellt - Batch-Datei zum Wechseln zwischen den Datenbanken (lokal und db4free)

---
+++ JS/ravi.helper.js
@@ -0,0 +1,23 @@
+// Dekodiert einen UTF8-kodierten String
+function rhDecodeUtf8(s) {
+  return decodeURIComponent(escape(s));
+}
+
+// Kapselung der MouseHover-Funktion für die Bilder der Diagramme im Menu
+$(function() {
+	$('.hover').hover(function(){
+		$('.infobox',this).toggle()
+	})
+})
+		
+// Testfunktion
+function anzeigen(){
+	var standort = $("#standort option:selected").text();
+	var haus = $("#haus option:selected").text();
+	var raum = $("#raum option:selected").text();
+	//var node = document.getElementById('pfad');
+	//var standortHeader = document.createTextNode(standort);
+	//node.parentNode.insertBefore(standortHeader, node );			
+	alert(standort+" "+haus+raum);
+	
+}
