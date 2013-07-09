c110c4bf5e2bd4983d2e8b6660c469157da7c746
Günay <guenay@internet.de>
2013-07-09 19:46:43 +0200

    Umlautproblem behoben

--- JS/ravi.menu.datumsauswahl.js
+++ JS/ravi.menu.datumsauswahl.js
@@ -8,11 +8,11 @@ $(function() {
 		
 		onSelect: function(dateText, inst) {
 			var startDate = new Date(dateText); 
-			//ausgew‰hltes Datum aus Datepicker einlesen
+			//ausgew√§hltes Datum aus Datepicker einlesen
 			var selDay = startDate.getDate(); 	
 			var selmonth=startDate.getMonth()+1; 
 			var selyear=startDate.getFullYear(); 
-			//einzelne DropDown-Men¸s mit eingelesenem Wert ‰ndern
+			//einzelne DropDown-Men√ºs mit eingelesenem Wert √§ndern
 			$("#Day").val(selDay); 					
 			$("#Month").val(selmonth); 				
 			$("#Year").val(selyear); 				
@@ -23,12 +23,12 @@ $(function() {
 // deutsche Initialisierung des Datepickers
 jQuery(function($){
 	$.datepicker.regional['de'] = {
-		 closeText: 'schlieﬂen',
-		 prevText: '&#x3c;zur¸ck',
+		 closeText: 'schlie√üen',
+		 prevText: '&#x3c;zur√ºck',
 		 nextText: 'Vor&#x3e;',
 		 currentText: 'heute',
-		 monthNames: ['Januar','Februar','M‰rz','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
-		 monthNamesShort: ['Jan','Feb','M‰r','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Dez'],
+		 monthNames: ['Januar','Februar','M√§rz','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
+		 monthNamesShort: ['Jan','Feb','M√§r','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Dez'],
 		 dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
 		 dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
 		 dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
@@ -40,26 +40,26 @@ jQuery(function($){
 	$.datepicker.setDefaults($.datepicker.regional['de']);
 });
 
-// On-Change-Funktion an Optionsmen¸s f¸r Datumpicker h‰ngen
+// On-Change-Funktion an Optionsmen√ºs f√ºr Datumpicker h√§ngen
 $(function () {
 	$("#Month").on("change", function () { BindDays();});
 	$("#Year").on("change", function () { BindDays();});
 	BindDays();
 });
 
-// On-Change-Funktion f¸r Datumsauswahl ¸ber Optionsmen¸s
+// On-Change-Funktion f√ºr Datumsauswahl √ºber Optionsmen√ºs
 function BindDays() {
-	var month = $("#Month").val(); //var month bekommt Wert d.ausgew‰hlten Monats aus Dropdown zugewiesen
-	var dayDropDown = $("#Day"); //var dayDropDown bekommt Wert d.ausgew‰hlten Tages aus Dropdown zugewiesen
+	var month = $("#Month").val(); //var month bekommt Wert d.ausgew√§hlten Monats aus Dropdown zugewiesen
+	var dayDropDown = $("#Day"); //var dayDropDown bekommt Wert d.ausgew√§hlten Tages aus Dropdown zugewiesen
 	dayDropDown.empty();
 	
 	if (month == 2) {   //Februar
 		for (var i = 1; i <= 28; i++) {
-			dayDropDown.append("<option value=" + i + ">" + i + "</option>"); //max Wert von i (28) wird angeh‰ngt
+			dayDropDown.append("<option value=" + i + ">" + i + "</option>"); //max Wert von i (28) wird angeh√§ngt
 		}
-		var year = $("#Year").val(); // Variable year bekommt Wert d. ausgew‰hlten Jahres aus Dropdown zugewiesen
+		var year = $("#Year").val(); // Variable year bekommt Wert d. ausgew√§hlten Jahres aus Dropdown zugewiesen
 		if (parseInt(year) % 4 == 0) {  //bei Schaltjahr
-			dayDropDown.append("<option value='29'>29</option>"); //29 wird im DropDown angeh‰ngt
+			dayDropDown.append("<option value='29'>29</option>"); //29 wird im DropDown angeh√§ngt
 		}
 	}	
 	else if (month == 4 || month == 6 || month == 9 || month == 11) { //Monate mit 30 Tagen
