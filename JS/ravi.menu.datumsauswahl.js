69f1de1f174fe7ce8d891182e24bf262817d6952
Günay <guenay@internet.de>
2013-07-09 22:31:46 +0200

    Änderungen an der Datumsauswahl: - Tag wird nicht mehr zurückgesetzt

--- JS/ravi.menu.datumsauswahl.js
+++ JS/ravi.menu.datumsauswahl.js
@@ -12,6 +12,16 @@ $(function() {
 			var selDay = startDate.getDate(); 	
 			var selmonth=startDate.getMonth()+1; 
 			var selyear=startDate.getFullYear(); 
+			completeDaysDropdown();			
+			if (selmonth == 2) {   //Februar
+				$("#Day option[value=30]").remove();
+				$("#Day option[value=31]").remove();
+				if (parseInt(selyear) % 4 != 0)
+					$("#Day option[value=29]").remove();
+			}
+			else if (selmonth == 4 || selmonth == 6 || selmonth == 9 || selmonth == 11)//Monate mit 30 Tagen
+				$("#Day option[value=31]").remove();	
+				
 			//einzelne DropDown-Menüs mit eingelesenem Wert ändern
 			$("#Day").val(selDay); 					
 			$("#Month").val(selmonth); 				
@@ -47,29 +57,35 @@ $(function () {
 	BindDays();
 });
 
+function completeDaysDropdown(){
+	var dayDropDown = $("#Day");
+	// Optionen werden hinzugefügt wenn sie nicht vorhanden sind
+	if ( $("#Day option[value=29]").length == 0 )
+		dayDropDown.append("<option value='29'>29</option>");
+	if ( $("#Day option[value=30]").length == 0 )
+		dayDropDown.append("<option value='30'>30</option>");
+	if ( $("#Day option[value=31]").length == 0 )
+		dayDropDown.append("<option value='31'>31</option>");
+};
+
+
 // On-Change-Funktion für Datumsauswahl über Optionsmenüs
 function BindDays() {
 	var month = $("#Month").val(); //var month bekommt Wert d.ausgewählten Monats aus Dropdown zugewiesen
 	var dayDropDown = $("#Day"); //var dayDropDown bekommt Wert d.ausgewählten Tages aus Dropdown zugewiesen
-	dayDropDown.empty();
+	var dayDropDownvalue = $("#Day").val(); // Wert des ausgewählten Tages
+	var year = $("#Year").val(); // Variable year bekommt Wert d. ausgewählten Jahres aus Dropdown zugewiesen
+
+	completeDaysDropdown();
 	
 	if (month == 2) {   //Februar
-		for (var i = 1; i <= 28; i++) {
-			dayDropDown.append("<option value=" + i + ">" + i + "</option>"); //max Wert von i (28) wird angehängt
-		}
-		var year = $("#Year").val(); // Variable year bekommt Wert d. ausgewählten Jahres aus Dropdown zugewiesen
-		if (parseInt(year) % 4 == 0) {  //bei Schaltjahr
-			dayDropDown.append("<option value='29'>29</option>"); //29 wird im DropDown angehängt
-		}
+		$("#Day option[value=30]").remove();
+		$("#Day option[value=31]").remove();
+		if (parseInt(year) % 4 != 0)
+			$("#Day option[value=29]").remove();
 	}	
 	else if (month == 4 || month == 6 || month == 9 || month == 11) { //Monate mit 30 Tagen
-		for (var i = 1; i <= 30; i++) {
-			dayDropDown.append("<option value=" + i + ">" + i + "</option>");
-		}
+		$("#Day option[value=31]").remove();	
+
 	}	
-	else {
-		for (var i = 1; i <= 31; i++) { //Monate mit 31 Tagen
-			dayDropDown.append("<option value=" + i + ">" + i + "</option>");
-		}
-	}
 }
