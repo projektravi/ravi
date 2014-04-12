355b2dfbe5842ec2b3751b2363d7c8c497458d3b
Lina <s_brunsl@stud.hwr-berlin.de>
2014-04-12 10:52:32 +0200

    Anpassung des Impressums, Anpassung der Breite der Div Container

--- JS/ravi.impressum.js
+++ JS/ravi.impressum.js
@@ -1,4 +1,4 @@
-var Neu= "<html><h1>Impressum </h1><b>Postanschrift:</b><br>Projektgruppe RaVi<br>Hochschule f&uuml;r Wirtschaft und Recht Berlin <br>Campus Lichtenberg <br>Alt-Friedrichsfelde 60 <br>10315 Berlin<br><p>&nbsp;</p><b>Verantwortliche:</b><br>Projektgruppe RaVi:<br>Bahar Aksoy, Yasemin Aksoylu, G&uuml;nay Aras, Lina Bruns, Frederic-Louis Claudius,<br>Margarita Elkina, Esra Koc, Marie Leser, Maik Lindner, Hoang Phuong Pham, Phillip Wenig <p>&nbsp;</p></html>";
+var Neu= "<html><h1>Impressum </h1><b>Postanschrift:</b><br>Projektgruppe RaVi<br>Hochschule f&uuml;r Wirtschaft und Recht Berlin <br>Campus Lichtenberg <br>Alt-Friedrichsfelde 60 <br>10315 Berlin<br><p>&nbsp;</p><b>Verantwortliche:</b><br><br>Projektgruppe RaVi:<br>Bahar Aksoy, Yasemin Aksoylu, G&uuml;nay Aras, Lina Bruns, Frederic-Louis Claudius,<br> Esra Koc, Marie Leser, Maik Lindner, Hoang Phuong Pham, Phillip Wenig <br><br>Projektleitung:</br> Frau Prof. Dr. Elkina<p>&nbsp;</p></html>";
 function changeDivContent(){
 	document.getElementById("grafik").innerHTML = Neu;
 }
--- index.php
+++ index.php
@@ -6,13 +6,12 @@
 <?php include "css/_includes" ?>	
 	<!-- Inkludierte Javascript-Dateien -->
 <?php include "js/_includes" ?>
-<style type="text/css">
- a:link    {color:black;}
- a:visited {color:black;}
- a:hover   {color:black;}
- a:active  {color:black;}
-</style>
-
+	<style type="text/css">
+		a:link    {color:black;}
+		a:visited {color:black;}
+		a:hover   {color:black;}
+		a:active  {color:black;}
+	</style>
 </head>
 <body> <!-- onLoad="rmrFrageStandorteAb()"><!--rmrFrageStandorteAb() -->
 
@@ -21,22 +20,18 @@
 				<a href="/"><img id="logo_ravi" src="Bilder/RaViLogok.jpg" /></a>
 				<a href="http://www.hwr-berlin.de" target="_blank"><img id="logo_hwr" src="Bilder/hwr-logo.png" /></a>
 			</div>
+			
 			<div   id="grau_div1">
 				<table border="0" cellpadding="7">
-				<colgroup>
-					<col width="90">
-					<col width="77">
-					<col width="200">
-				</colgroup>
 					<tr>
 						<td><a href="index.php">Startseite</td>
 						<td>Kontakt</td>
 						<td><a href="javascript:changeDivContent()">Impressum/Team</td>
-						
 					</tr>
 				</table>			
 			</div>
-			
+	</div>		
+	
 	<div id="content">
 		<div id="navigation">
 			<div id="menue_div1"><a class="upperHead">Navigation<a></div>
