ea81fd422ca086e0712bd2debf8ada50beb58c3f
Phillip <s_wenigp@stud.hwr-berlin.de>
2014-06-05 15:56:34 +0200

    Pfad_impr_kont
    Pfad ändert sich bei Klick in Navigation

--- JS/ravi.kontakt.js
+++ JS/ravi.kontakt.js
@@ -3,4 +3,5 @@ var str = " margarita.elkina@hwr-berlin.de";
 var Neu1=  "<p></p> Haben Sie Fragen, Wünsche oder benötigen Sie noch zusätzliche Informationen zu RaVi, dann schreiben Sie uns einfach eine E-Mail an:</p>"
 function changeDivKontakt(){
 	document.getElementById("grafik").innerHTML =  Neu1 + result;
+	$("#pfad").html("<a class='upperHead routeMap'>Kontakt</a>");
 }
